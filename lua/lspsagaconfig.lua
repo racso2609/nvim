49d51e4635f44395b235d99d1407550e8f7ee2ab
oscar lugo <oscaremiliolugo@gmail.com>
2021-11-29 17:40:15 +0000

    lspsaga for lsp

--- lua/lspsagaconfig.lua
+++ lua/lspsagaconfig.lua
@@ -10,6 +10,8 @@ saga.init_lsp_saga{
 }
 
 map('','<C-j>', '<cmd>Lspsaga diagnostic_jump_next<cr>');
-map('','k', '<cmd>Lspsaga Hover_doc<cr>');
-map('','<c-k>', '<cmd>Lspsaga signature_help<cr>');
+map('','k', '<cmd>lua vim.lsp.buf.hover()<cr>');
+map('','<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>');
 map('','gh', '<cmd>Lspsaga lsp_finder<cr>');
+map('','re', '<cmd>Lspsaga rename<cr>');
+
