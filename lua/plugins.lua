d9139cc0a13a0b69562d336c36229926e4bd4742
Oscar <oscaremiliolugo@gmail.com>
2022-02-03 23:07:57 -0500

    update plugin file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,56 +4,61 @@ local fn = vim.fn
 
 
 cmd 'packadd paq-nvim'               -- load the package manager
-local paq = require('paq-nvim').paq  -- a convenient alias
+
+require('paq') -- a convenient alias
+{
 -- color schemas
--- paq {'folke/tokyonight.nvim'}
--- paq {'tjdevries/colorbuddy.vim'}
--- paq {"Th3Whit3Wolf/onebuddy"}
-paq 'Matsuuu/pinkmare'
+--  {'folke/tokyonight.nvim'}
+--  {'tjdevries/colorbuddy.vim'}
+--  {"Th3Whit3Wolf/onebuddy"}
+ 'Matsuuu/pinkmare';
 
-paq 'sbdchd/neoformat'
+ 'sbdchd/neoformat';
 
-paq {'savq/paq-nvim', opt = true}    -- paq-nvim manages itself
-paq {'shougo/deoplete-lsp'}
-paq {'shougo/deoplete.nvim', run = fn['remote#host#UpdateRemotePlugins']}
-paq {'tbodt/deoplete-tabnine', run = './install.sh' }
--- paq {'neoclide/coc.nvim', run = fn['yarn install --frozen-lockfile']}
+ {'savq/paq-nvim', opt = true};    -- paq-nvim manages itself
+ {'shougo/deoplete-lsp'};
+ {'shougo/deoplete.nvim', run = fn['remote#host#UpdateRemotePlugins']};
+ {'tbodt/deoplete-tabnine', run = './install.sh' };
+--  {'neoclide/coc.nvim', run = fn['yarn install --frozen-lockfile']}
 
 -- tree sitter and plugins
-paq {'nvim-treesitter/nvim-treesitter'}
-paq 'romgrk/nvim-treesitter-context'
-paq 'windwp/nvim-ts-autotag'
-paq 'voldikss/vim-floaterm'
-paq 'mattn/emmet-vim'
-paq 'terrortylor/nvim-comment'
-paq 'kyazdani42/nvim-tree.lua'
-paq 'jiangmiao/auto-pairs'
-paq 'glepnir/lspsaga.nvim'
-paq 'vimwiki/vimwiki'
+ {'nvim-treesitter/nvim-treesitter'};
+ 'romgrk/nvim-treesitter-context';
+ 'windwp/nvim-ts-autotag';
+ 'voldikss/vim-floaterm';
+ 'mattn/emmet-vim';
+ 'terrortylor/nvim-comment';
+ 'kyazdani42/nvim-tree.lua';
+ 'jiangmiao/auto-pairs';
+ 'glepnir/lspsaga.nvim';
+ 'vimwiki/vimwiki';
 
 
-paq {'neovim/nvim-lspconfig'}
-paq {'junegunn/fzf', run = './install --bin'}
-paq {'junegunn/fzf.vim'}
-paq {'ibhagwan/fzf-lua'}
-paq {'kyazdani42/nvim-web-devicons'}
-paq {'ojroques/nvim-lspfuzzy'}
+ {'neovim/nvim-lspconfig'};
+ {'junegunn/fzf', run = './install --bin'};
+ {'junegunn/fzf.vim'};
+ {'ibhagwan/fzf-lua'};
+ {'kyazdani42/nvim-web-devicons'};
+ {'ojroques/nvim-lspfuzzy'};
 
-paq 'SirVer/ultisnips'
-paq 'honza/vim-snippets'
+ 'SirVer/ultisnips';
+ 'honza/vim-snippets';
 
 
 ------ ident guide
-paq {'lukas-reineke/indent-blankline.nvim'}
+ {'lukas-reineke/indent-blankline.nvim'};
 
 --   ----- css
-paq 'tree-sitter/tree-sitter-css'
-paq 'norcalli/nvim-colorizer.lua'
+ 'tree-sitter/tree-sitter-css';
+ 'norcalli/nvim-colorizer.lua';
 
 ------ ts and js --------
-paq {'HerringtonDarkholme/yats.vim'}                -- "ts syntax
-paq {'mxw/vim-jsx'}                                 --"jsx syntax}
-paq {'styled-components/vim-styled-components'}
-paq {'mlaursen/vim-react-snippets'}
+ {'HerringtonDarkholme/yats.vim'} ;               -- "ts syntax
+ {'mxw/vim-jsx'};              --"jsx syntax}
+ {'styled-components/vim-styled-components'};
+ {'mlaursen/vim-react-snippets'};
 
+
+
+}
 g['deoplete#enable_at_startup'] = 1  -- enable deoplete at startup
