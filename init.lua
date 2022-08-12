31029dd3ee8241e2c973f65a66f1f2ec264c8f48
racso2609 <oscaremiliolugo@gmail.com>
2022-08-11 23:14:22 -0400

    add lua formatting

--- init.lua
+++ init.lua
@@ -3,3 +3,15 @@ require 'highlights'
 require 'plugins'
 require 'keymaps'
 
+Lua = {
+  format = {
+    enable = true,
+    -- Put format options here
+    -- NOTE: the value should be STRING!!
+    defaultConfig = {
+      indent_style = "space",
+      indent_size = "2",
+    }
+  },
+}
+
