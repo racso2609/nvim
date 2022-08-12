0bcd5d5454a4a7993884faa0a80d9ea2011f3082
racso2609 <oscaremiliolugo@gmail.com>
2022-08-11 21:54:12 -0400

    fix statup sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,23 +14,27 @@ fi
 # Use package feature to install coc.nvim
 
 # for vim8
-mkdir -p ~/.vim/pack/coc/start
-cd ~/.vim/pack/coc/start
-curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
-# for neovim
-# mkdir -p ~/.local/share/nvim/site/pack/coc/start
-# cd ~/.local/share/nvim/site/pack/coc/start
+# mkdir -p ~/.vim/pack/coc/start
+# cd ~/.vim/pack/coc/start
 # curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
+# for neovim
+mkdir -p ~/.local/share/nvim/site/pack/coc/start
+cd ~/.local/share/nvim/site/pack/coc/start
+curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
 
 # Install extensions
 mkdir -p ~/.config/coc/extensions
 cd ~/.config/coc/extensions
 if [ ! -f package.json ]
 then
-  echo '{"dependencies":{}}'> package.json
+  rm -r ~/.config/coc
+  cd ~/.config
+  git clone https://github.com/racso2609/coc-config
+  mv coc-config ./coc
+  # echo '{"dependencies":{}}'> package.json
 fi
 # Change extension names to the extensions you need
-npm install coc-snippets --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
+# npm install coc-snippets --global-style --ignore-scripts --no-bin-links --no-package-lock --only=prod
 
 #install paq
 git clone --depth=1 https://github.com/savq/paq-nvim.git \
