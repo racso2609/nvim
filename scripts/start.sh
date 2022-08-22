666cc9056515658984d99a3df6fb69d3f011d3fa
racso2609 <oscaremiliolugo@gmail.com>
2022-08-22 10:01:24 -0400

    add lsp efm server to setup script

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,6 +9,7 @@ if [ ! -x "$(command -v node)" ]; then
     export PATH="/usr/local/bin/:$PATH"
     # Or use package manager, e.g.
     # sudo apt-get install nodejs
+    npm i -g neovim 
 fi
 
 # Use package feature to install coc.nvim
@@ -39,3 +40,7 @@ fi
 #install paq
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# -- install efm languagge server
+sudo pacman -S go
+go install github.com/mattn/efm-langserver@latest
