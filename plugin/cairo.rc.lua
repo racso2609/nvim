55d46b1f4b60b925e519cbf132ac58d041af40d2
racso2609 <oscaremiliolugo@gmail.com>
2022-08-23 00:33:44 -0400

    disable coc automatically on init

--- plugin/cairo.rc.lua
+++ plugin/cairo.rc.lua
@@ -1,10 +1,14 @@
 local autocmd = vim.api.nvim_create_autocmd
+local g = vim.g
+
 vim.g.cairo_linter_autosave = 1
+g.coc_start_at_startup = false
 
 autocmd("FileType", {
   pattern = "cairo",
   callback = function()
     print("cairo")
     require("cmp").setup.buffer { enabled = false }
+    -- g.
   end,
 })
