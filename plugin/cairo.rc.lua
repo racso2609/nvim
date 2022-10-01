91243e849aa67e2a6d99fecd4511eef12ebbc9ec
racso2609 <oscaremiliolugo@gmail.com>
2022-10-01 16:48:05 -0400

    add cairo support

--- plugin/cairo.rc.lua
+++ plugin/cairo.rc.lua
@@ -1,33 +1,10 @@
--- local autocmd = vim.api.nvim_create_autocmd
--- local CAIRO_BASE_URI = "/home/racso/Work/nexera/MetaNFT-Cairo/"
 local status, cairo = pcall(require, "cairo-support-nvim")
 if not status then
-	return
+  return
 end
+
 cairo.setup({
-	format = true,
-	compile = true,
+  format = true,
+  compile = true,
+  environment = "nexera_cairo",
 })
--- g.coc_start_at_startup = false
-
--- autocmd("BufWritePost", {
--- 	pattern = "*.cairo",
--- 	callback = function()
--- 		print("cairo")
--- 		vim.cmd("!cd " .. CAIRO_BASE_URI)
---
--- 		local completeFilePath = vim.fn.expand("%:p")
--- 		local filePath = string.sub(completeFilePath, 39)
--- 		print(filePath)
--- 		vim.cmd("!starknet-compile " .. filePath)
--- 	end,
--- })
-
--- autocmd("FileWritePost", {
--- 	pattern = "cairo",
--- 	callback = function()
--- 		print("cairo")
--- 		local filePath = vim.fn.expand("%:p")
--- 		vim.cmd("!protostar build " .. filePath)
--- 	end,
--- })
