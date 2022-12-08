775876c8eae8a6a92da25ce2c8f71a2c6df8bbfe
racso2609 <oscaremiliolugo@gmail.com>
2022-12-08 18:28:42 -0400

    disable null-ls

--- plugin/null-ls.rc.lua
+++ plugin/null-ls.rc.lua
@@ -1,50 +1,52 @@
 local autocmd = vim.api.nvim_create_autocmd
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-	return
+  return
 end
 
 local null_config = {
-	sources = {
-		-- lua
-		null_ls.builtins.formatting.stylua.with({ filetypes = { "lua" } }),
+  sources = {
+    -- lua
+    -- null_ls.builtins.formatting.stylua.with({ filetypes = { "lua" } }),
 
-		-- html
-		null_ls.builtins.formatting.djlint,
-		---    null_ls.builtins.formatting.astyle,
+    -- html
+    -- null_ls.builtins.formatting.djlint,
+    -- -   null_ls.builtins.formatting.astyle,
 
-		-- js
-		null_ls.builtins.diagnostics.eslint,
-		null_ls.builtins.formatting.prettier,
+    -- js
+    null_ls.builtins.diagnostics.eslint,
+    -- null_ls.builtins.code_actions.eslint,
+    -- null_ls.builtins.code_actions.xo,
+    -- null_ls.builtins.formatting.prettier,
 
-		-- proto
-		null_ls.builtins.formatting.protolint,
+    -- proto
+    -- null_ls.builtins.formatting.protolint,
 
-		-- spell
-		null_ls.builtins.completion.spell,
-		null_ls.builtins.diagnostics.codespell.with({ filetypes = {} }),
+    -- spell
+    null_ls.builtins.completion.spell,
+    null_ls.builtins.diagnostics.codespell.with({ filetypes = {} }),
 
-		-- general
-		null_ls.builtins.formatting.trim_newlines,
-		null_ls.builtins.formatting.trim_whitespace,
-	},
-	on_attach = function(client)
-		if client.server_capabilities.documentFormattingProvider then
-			vim.keymap.set("n", "<Leader>f", function()
-				vim.lsp.buf.format({ async = false })
-			end)
+    -- general
+    null_ls.builtins.formatting.trim_newlines,
+    null_ls.builtins.formatting.trim_whitespace,
+  },
+  on_attach = function(client)
+    if client.server_capabilities.documentFormattingProvider then
+      vim.keymap.set("n", "<Leader>f", function()
+        vim.lsp.buf.format({ async = false })
+      end)
 
-			-- format on save
-			autocmd("BufWritePost", {
-				callback = function()
-					vim.lsp.buf.format({ async = false })
-				end,
-			})
-		end
+      -- format on save
+      autocmd("BufWritePost", {
+        callback = function()
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
+    end
 
-		if client.server_capabilities.documentRangeFormattingProvider then
-			vim.keymap.set("v", "<Leader>f", vim.lsp.buf.range_formatting)
-		end
-	end,
+    if client.server_capabilities.documentRangeFormattingProvider then
+      vim.keymap.set("v", "<Leader>f", vim.lsp.buf.range_formatting)
+    end
+  end,
 }
 null_ls.setup(null_config)
---
+++ plugin/prettier.rc.lua
@@ -0,0 +1,30 @@
+ local prettier = require("prettier")
+ -- local exist = prettier.config_exists({ check_package_json = true })
+ 
+ prettier.setup({
+   bin = 'prettier', -- or `'prettierd'` (v0.22+)
+   filetypes = {
+     "css",
+     "graphql",
+     "html",
+     "javascript",
+     "javascriptreact",
+     "json",
+     "less",
+     "markdown",
+     "scss",
+     "typescript",
+     "typescriptreact",
+     "yaml",
+   },
+   ["null-ls"] = {
+     condition = function()
+      return true
+     end,
+     -- runtime_condition = function(params)
+     -- return false to skip running prettier
+     -- return true
+     -- end,
+     timeout = 5000
+   }
+ })
