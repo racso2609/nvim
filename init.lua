fb4e0b339c173eb6a0858dc019f977bc591fe3be
racso2609 <oscaremiliolugo@gmail.com>
2023-01-09 00:07:48 -0400

    config imports

---
+++ init.lua
@@ -0,0 +1,28 @@
+local foldersTable = {
+	"core",
+	"core.autocmd",
+	"core.plugin",
+	"core.base",
+	"core.keymaps",
+}
+
+ for _, folder in ipairs(foldersTable) do
+ 	local status, fail = pcall(require, folder)
+ 	if not status then
+ 		vim.api.nvim_err_writeln(
+ 			"Failed to load "
+ 				.. folder
+ 				.. "\n\n"
+ 				.. fail
+ 		)
+ 	end
+ end
+
+ if vim.fn.has("nvim-0.8") ~= 1 or vim.version().prerelease then
+ 	vim.schedule(function()
+ 		racsonvim.notify(
+ 			"Unsupported Neovim Version! Please check the requirements",
+ 			"error"
+ 		)
+ 	end)
+ end
