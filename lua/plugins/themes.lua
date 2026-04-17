3fd16e34c9dcf1a20fa1c9515e7995350a6177a1
racso2609 <oscaremiliolugo@gmail.com>
2026-04-17 13:55:11 -0400

    add mini icosn

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -2,5 +2,12 @@ return {
   -- "rafamadriz/neon",
   -- "Mofiqul/vscode.nvim",
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "echasnovski/mini.icons",
+    version = false,
+    config = function()
+      require("mini.icons").setup()
+    end,
+  },
   -- "ribru17/bamboo.nvim",
 }
